bc596914b0fddc00fadabac6e329729684cdbce7
Andrey529 <andr02eyes@gmail.com>
2023-04-11 16:26:41 +0300

    added pseudo json controller

--- src/main/java/com/example/testspringapp/controller/Controller.java
+++ src/main/java/com/example/testspringapp/controller/Controller.java
@@ -1,9 +1,17 @@
 package com.example.testspringapp.controller;
 
 import com.example.testspringapp.TestSpringAppApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class Controller {
@@ -19,5 +27,14 @@ public class Controller {
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
+    @GetMapping("/json")
+    public ResponseEntity<?> create() {
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        data.put("key2", "value2");
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
 
 }
