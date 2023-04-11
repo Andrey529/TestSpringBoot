7c1d08bdb48be3569d4b0c578999e6e0bb44fce1
Andrey529 <andr02eyes@gmail.com>
2023-04-11 16:53:06 +0300

    added cross origin

--- src/main/java/com/example/testspringapp/controller/Controller.java
+++ src/main/java/com/example/testspringapp/controller/Controller.java
@@ -3,6 +3,7 @@ package com.example.testspringapp.controller;
 import com.example.testspringapp.TestSpringAppApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 public class Controller {
     TestSpringAppApplication backendService = new TestSpringAppApplication();
